923ba485c8c29b10fbebeacdc62fa07a4f46e232
Ben <bboksner@LAPTOP-0U0LFUE0.localdomain>
2022-12-16 19:00:35 -0500

    set up top level state and fetched data from db.json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-function App() {
+import {React,useState,useEffect} from 'react'
+import {Routes,Route} from 'react-router-dom'
+const [cards,setCards] = useState([{coverUrl:"",artistName:"",songName:"",tabLink:"",id:0}]);
+  useEffect(() => {
+    fetch('http://localhost:3001/cards')
+    .then((response) => response.json())
+    .then(e=>{
+        console.log('success :)',e)
+        setCards(e)
+    })
+}, [])
+export default function App() {
   return (
-    <div className="App">
-     
-    </div>
-  );
+    <div>App</div>
+  )
 }
-
-export default App;
