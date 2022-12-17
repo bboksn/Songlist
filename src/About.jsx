956f4a33b4f159b750e4c998c6751ea5571bdd20
Ben <bboksner@LAPTOP-0U0LFUE0.localdomain>
2022-12-16 20:36:07 -0500

    added about page data

---
+++ src/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='border-black shadow-sm shadow-black'>
+        <h1 className='text-3xl'>Song List and GuitarTab App</h1>
+        <h2 className='text-2xl'>input a song, its details, and links to tab or youtube and see your list of songs!</h2>
+        <h3 className='text-xl'>This project was made using React, ReactRouterV6, Json-Server, and TailwindCSS</h3>
+    </div>
+  )
+}
