f58af8f6d519da3b0aa64c3ff8e4199a677405f8
Ben <bboksner@LAPTOP-0U0LFUE0.localdomain>
2022-12-16 19:26:08 -0500

    mapped card components to card page

--- src/Cardspage.jsx
+++ src/Cardspage.jsx
@@ -3,6 +3,12 @@ import Card from './Card'
 
 export default function Cardspage() {
   return (
-    <div>Cardspage</div>
+    <>
+    <div className='flex flex-wrap'>
+    {cards.map(e => {
+     return <Card coverUrl={e.coverUrl} artistName={e.artistName} songName={e.songName} tabLink={e.tabLink} key={e.id}></Card>  
+    })}
+    </div>
+    </>
   )
 }
