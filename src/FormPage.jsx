1855536c2a757f7b8727ffc685bef82ae184338d
Ben <bboksner@LAPTOP-0U0LFUE0.localdomain>
2022-12-16 20:16:06 -0500

    added empty functions for controlled form handling

--- src/FormPage.jsx
+++ src/FormPage.jsx
@@ -4,6 +4,12 @@ import { useNavigate } from 'react-router-dom';
 export default function FormPage() {
     const [formData,setFormData] = useState({});
     const navigate = useNavigate();
+    function fillForm(e){
+
+    }
+    function handleForm(e){
+        
+    }
   return (
     <div>FormPage</div>
   )
