282820d0ecad8d93cd9af348aa189cff6b0eb9aa
Ben <bboksner@LAPTOP-0U0LFUE0.localdomain>
2022-12-16 19:28:08 -0500

    imported cardspage to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {React,useState,useEffect} from 'react'
 import {Routes,Route} from 'react-router-dom'
 import Nav from './Nav';
+import Cardspage from './Cardspage';
 export default function App() {
   const [cards,setCards] = useState([{coverUrl:"",artistName:"",songName:"",tabLink:"",id:0}]);
   useEffect(() => {
