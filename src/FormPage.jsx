5939503fb0a76217a17a2f633e4037d78bacf802
Ben <bboksner@LAPTOP-0U0LFUE0.localdomain>
2022-12-16 20:14:28 -0500

    set up state and navigate vars

--- src/FormPage.jsx
+++ src/FormPage.jsx
@@ -2,6 +2,8 @@ import {React,useState} from 'react'
 import { useNavigate } from 'react-router-dom';
 
 export default function FormPage() {
+    const [formData,setFormData] = useState({});
+    const navigate = useNavigate();
   return (
     <div>FormPage</div>
   )
